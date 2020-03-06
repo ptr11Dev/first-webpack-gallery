4da2e8c6e49693e242797747b0f2a516c1b62e87
id github: ptr11Dev <piotrkaczmarek.dev@gmail.com>
2020-03-06 13:52:52 +0100

    create neccessary functions

--- js/app.js
+++ js/app.js
@@ -1,1 +1,20 @@
 import "../sass/style.scss";
+
+const apiUrl = "https://dog.ceo/api";
+
+const listBreeds = () => {
+  return fetch(`${apiUrl}/breeds/list/all`)
+    .then(res => res.json())
+    .then(data => data.message);
+};
+
+const getRandomImage = () => {
+  return fetch(`${apiUrl}/breeds/image/random`)
+    .then(res => res.json())
+    .then(data => data.message);
+};
+const getRandomImageByBreed = breed => {
+  return fetch(`${apiUrl}/breed/${breed}/images/random`)
+    .then(res => res.json())
+    .then(img => img.message);
+};
