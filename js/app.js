aa490b14802463153654bc9b87705575b28ba62e
id github: ptr11Dev <piotrkaczmarek.dev@gmail.com>
2020-03-06 15:49:09 +0100

    add whole project

--- js/app.js
+++ js/app.js
@@ -3,9 +3,16 @@ import "../sass/style.scss";
 class Doge {
   constructor() {
     this.apiUrl = "https://dog.ceo/api";
-    this.imgEl = document.querySelector(".featured-dog img");
+    this.imgEl = document.querySelector(".random-dog img");
+    this.bgcEl = document.querySelector(".random-dog__background");
+    this.tilesEl = document.querySelector(".tiles");
+    this.spinnerEl = document.querySelector(".spinner");
 
     this.init();
+  }
+
+  toggleLoading() {
+    this.spinnerEl.classList.toggle("spinner-visible");
   }
 
   listBreeds() {
@@ -26,10 +33,61 @@ class Doge {
       .then(img => img.message);
   }
 
-  init() {
-    this.getRandomImage().then(img => this.imgEl.setAttribute("src", img));
+  showAllBreeds() {
+    this.listBreeds().then(breeds => {
+      for (const breed in breeds) {
+        if (breeds[breed].length === 0) {
+          this.addBreed(breed);
+        } else {
+          for (const subBreed of breeds[breed]) {
+            this.addBreed(breed, subBreed);
+          }
+        }
+      }
+    });
+  }
 
-    this.listBreeds().then(breeds => console.log(breeds));
+  showImageWhenReady(image) {
+    this.imgEl.setAttribute("src", image);
+    this.bgcEl.style.backgroundImage = `url("${image}")`;
+    this.toggleLoading();
+  }
+
+  addBreed(breed, subBreed) {
+    let name;
+    let type;
+
+    if (typeof subBreed === "undefined") {
+      name = breed;
+      type = breed;
+    } else {
+      name = `${breed} ${subBreed}`;
+      type = `${breed}/${subBreed}`;
+    }
+    const tile = document.createElement("div");
+    tile.classList.add("tiles__tile");
+    const tileContent = document.createElement("div");
+    tileContent.classList.add("tiles__tile-content");
+    tileContent.innerText = name;
+
+    tileContent.addEventListener("click", () => {
+      document.body.scrollIntoView({
+        behavior: "smooth"
+      });
+      this.toggleLoading();
+      this.getRandomImageByBreed(type).then(img =>
+        this.showImageWhenReady(img)
+      );
+    });
+    tile.appendChild(tileContent);
+    this.tilesEl.appendChild(tile);
+  }
+
+  init() {
+    this.toggleLoading();
+    this.getRandomImage().then(img => this.showImageWhenReady(img));
+
+    this.showAllBreeds();
   }
 }
 
